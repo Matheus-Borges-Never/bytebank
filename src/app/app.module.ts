4050a2ccf06f7c72bf9503b94af054704d56d75a
Matheus-Borges-Never <matheusborgespaulino@gmail.com>
2022-08-30 10:42:20 -0300

    transferencia do formulario para o component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BytebankComponent } from './bytebank/bytebank.component';
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BytebankComponent
+    BytebankComponent,
+    NovaTransferenciaComponent
   ],
   imports: [
     BrowserModule,
