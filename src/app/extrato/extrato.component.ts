acb5bcde0f7c9282b9a8dc085481ce554a809cf3
Matheus-Borges-Never <matheusborgespaulino@gmail.com>
2022-08-31 09:57:35 -0300

    Service criado

--- src/app/extrato/extrato.component.ts
+++ src/app/extrato/extrato.component.ts
@@ -1,3 +1,4 @@
+import { TransferenciaService } from './../service/transferencia.service';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
@@ -6,9 +7,11 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./extrato.component.scss'],
 })
 export class ExtratoComponent implements OnInit {
-  @Input() transferencias: any[];
+  transferencias: any[];
 
-  constructor() {}
+  constructor(private service: TransferenciaService) { }
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.transferencias = this.service.transferencias;
+  }
 }
